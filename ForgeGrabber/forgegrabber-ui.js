b9f391d8ddfc41553fb33db16747b6b0cd4b5843
ForgeCoreIndustries <james@forgecoreindustries.com>
2026-09-19 00:44:43 -0400

    Add files via upload

---
+++ ForgeGrabber/forgegrabber-ui.js
@@ -0,0 +1,19 @@
+import { ForgeGrabber } from "./forgegrabber-client.js";
+
+const out = document.getElementById("fgOutput");
+
+document.getElementById("fgPingBtn").onclick = async () => {
+  out.innerText = JSON.stringify(await ForgeGrabber.ping(), null, 2);
+};
+
+document.getElementById("fgGrabBtn").onclick = async () => {
+  out.innerText = JSON.stringify(await ForgeGrabber.grab(), null, 2);
+};
+
+document.getElementById("fgStatusBtn").onclick = async () => {
+  out.innerText = JSON.stringify(await ForgeGrabber.status(), null, 2);
+};
+
+document.getElementById("fgResultsBtn").onclick = async () => {
+  out.innerText = JSON.stringify(await ForgeGrabber.results(), null, 2);
+};
